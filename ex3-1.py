a86de221710d9b439455f9012d9ecd5f2af13707
gobuzzika@gmail.com <gobuzzika@gmail.com>
2019-09-09 09:18:33 +0530

    completed exercise 3-1 from thinkpython book

---
+++ ex3-1.py
@@ -0,0 +1,7 @@
+def right_justify(s):
+    lengthString = len(s)
+    newString = (70-lengthString)*' '+ s
+    return newString
+
+print (right_justify('Satyawrat'))
+print (right_justify('Janghu'))
