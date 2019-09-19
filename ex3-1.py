0ec7d92872889910831e9008fb1b6a7ba0c316cb
gosatya <gobuzzika@gmail.com>
2019-09-20 04:41:21 +0530

    installed pycharm. this commit contains pycharm project config files.

--- ex3-1.py
+++ ex3-1.py
@@ -1,7 +1,8 @@
 def right_justify(s):
     lengthString = len(s)
-    newString = (70-lengthString)*' '+ s
+    newString = (70 - lengthString) * ' ' + s
     return newString
 
-print (right_justify('Satyawrat'))
-print (right_justify('Janghu'))
+
+print(right_justify('Satyawrat'))
+print(right_justify('Janghu'))
